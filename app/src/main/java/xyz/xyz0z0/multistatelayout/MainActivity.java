27da985d4603901eed1e61ae869f770347bd9416
xyz0z0 <xyz0z0.xyz@gmail.com>
2020-04-13 17:01:17 +0800

    Update show empty and error

--- app/src/main/java/xyz/xyz0z0/multistatelayout/MainActivity.java
+++ app/src/main/java/xyz/xyz0z0/multistatelayout/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import xyz.xyz0z0.multistate.MultiStateLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     private MultiStateLayout mslMain;
     private Button btnShowContent;
     private Button btnShowEmpty;
+    private Button btnShowEmptyRetry;
+    private Button btnShowError;
+    private Button btnShowErrorRetry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnShowContent = findViewById(R.id.btn_show_content);
         btnShowEmpty = findViewById(R.id.btn_show_empty);
+        btnShowEmptyRetry = findViewById(R.id.btn_show_empty_retry);
+        btnShowError = findViewById(R.id.btn_show_error);
+        btnShowErrorRetry = findViewById(R.id.btn_show_error_retry);
         mslMain = findViewById(R.id.msl_main);
         btnShowContent.setOnClickListener(v -> {
             mslMain.showContent();
         });
+        btnShowError.setOnClickListener(v -> mslMain.showError());
+        btnShowErrorRetry.setOnClickListener(v -> mslMain
+            .showError(R.drawable.ic_launcher_foreground, getString(R.string.error), "点击重试", new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    Toast.makeText(view.getContext(), "点击重试了1", Toast.LENGTH_SHORT).show();
+                }
+            }));
         btnShowEmpty.setOnClickListener(v -> {
-            mslMain.showEmpty("没有更多数据", new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "重试", Toast.LENGTH_SHORT).show();
+            mslMain.showEmpty();
+        });
+        btnShowEmptyRetry.setOnClickListener(v -> {
+            mslMain.showEmpty(R.drawable.ic_launcher_foreground, getString(R.string.empty), "点击重试", new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    Toast.makeText(view.getContext(), "点击重试了2", Toast.LENGTH_SHORT).show();
                 }
             });
         });
--- multistate/src/main/java/xyz/xyz0z0/multistate/MultiStateLayout.java
+++ multistate/src/main/java/xyz/xyz0z0/multistate/MultiStateLayout.java
@@ -5,14 +5,16 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MultiStateLayout extends FrameLayout {
 
+    public static final int ID_NULL = 0;
     private LayoutInflater inflater;
     private View currentShowingView;
     private View contentView;
@@ -54,11 +56,19 @@ public class MultiStateLayout extends FrameLayout {
     }
 
     public void showLoading() {
+        showLoading(null);
+    }
+
+    public void showLoading(String loadingTip) {
         if (progressView == null) {
             progressView = inflater.inflate(R.layout.view_progress, this, false);
             ProgressBar progressBar = progressView.findViewById(R.id.progress_wheel);
             View progressContentView = progressView.findViewById(R.id.progress_content);
             addView(progressView);
+        }
+        if (loadingTip != null && loadingTip.length() > 0) {
+            TextView tvLoading = emptyView.findViewById(R.id.tv_loading);
+            tvLoading.setText(loadingTip);
         }
         currentShowingView.setVisibility(INVISIBLE);
         progressView.setVisibility(VISIBLE);
@@ -66,39 +76,75 @@ public class MultiStateLayout extends FrameLayout {
     }
 
     public void showError() {
-        showError(null);
+        showError(R.drawable.ic_error_18dp, getContext().getString(R.string.error), null, null);
     }
 
-    public void showError(OnClickListener listener) {
+    public void showError(@DrawableRes int resId, String error, String retry, OnClickListener listener) {
         if (errorView == null) {
             errorView = inflater.inflate(R.layout.view_error, this, false);
-            View errorContentView = errorView.findViewById(R.id.error_content);
-            TextView errorTextView = errorView.findViewById(R.id.errorTextView);
             addView(errorView);
         }
+        ImageView ivError = errorView.findViewById(R.id.iv_error);
+        TextView tvError = errorView.findViewById(R.id.tv_error);
+        TextView tvErrorRetry = errorView.findViewById(R.id.tv_error_retry);
+        if (resId != ID_NULL) {
+            ivError.setVisibility(VISIBLE);
+            ivError.setImageResource(resId);
+        } else {
+            ivError.setVisibility(GONE);
+        }
+        if (error != null) {
+            tvError.setVisibility(VISIBLE);
+            tvError.setText(error);
+        } else {
+            tvError.setVisibility(GONE);
+        }
+        if (retry != null) {
+            tvErrorRetry.setVisibility(VISIBLE);
+            tvErrorRetry.setText(retry);
+        } else {
+            tvErrorRetry.setVisibility(GONE);
+        }
         if (listener != null) {
-            errorView.setOnClickListener(listener);
+            tvErrorRetry.setOnClickListener(listener);
         }
         currentShowingView.setVisibility(INVISIBLE);
         errorView.setVisibility(VISIBLE);
         currentShowingView = errorView;
     }
 
-    public void showEmpty(String tip, OnClickListener listener) {
+    public void showEmpty() {
+        showError(R.drawable.ic_empty_18dp, getContext().getString(R.string.empty), null, null);
+    }
+
+    public void showEmpty(@DrawableRes int resId, String empty, String retry, OnClickListener listener) {
         if (emptyView == null) {
             emptyView = inflater.inflate(R.layout.view_empty, this, false);
-            View emptyContentView = emptyView.findViewById(R.id.empty_content);
-            TextView emptyTextView = emptyView.findViewById(R.id.tv_empty);
             addView(emptyView);
         }
-        if (tip != null && tip.length() > 0) {
-            TextView emptyTextView = emptyView.findViewById(R.id.tv_empty);
-            emptyTextView.setText(tip);
+        ImageView ivEmpty = emptyView.findViewById(R.id.iv_empty);
+        TextView tvEmpty = emptyView.findViewById(R.id.tv_empty);
+        TextView tvEmptyRetry = emptyView.findViewById(R.id.tv_empty_retry);
+        if (resId != ID_NULL) {
+            ivEmpty.setVisibility(VISIBLE);
+            ivEmpty.setImageResource(resId);
+        } else {
+            ivEmpty.setVisibility(GONE);
+        }
+        if (empty != null) {
+            tvEmpty.setVisibility(VISIBLE);
+            tvEmpty.setText(empty);
+        } else {
+            tvEmpty.setVisibility(GONE);
+        }
+        if (retry != null) {
+            tvEmptyRetry.setVisibility(VISIBLE);
+            tvEmptyRetry.setText(retry);
+        } else {
+            tvEmptyRetry.setVisibility(GONE);
         }
         if (listener != null) {
-            emptyView.findViewById(R.id.tv_retry).setOnClickListener(listener);
-        } else {
-            emptyView.findViewById(R.id.tv_retry).setVisibility(INVISIBLE);
+            tvEmptyRetry.setOnClickListener(listener);
         }
         currentShowingView.setVisibility(INVISIBLE);
         emptyView.setVisibility(VISIBLE);
